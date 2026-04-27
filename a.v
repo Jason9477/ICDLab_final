32bd60d19f172baaf69dfe17c85cd8b9e515d933
Jason Hsieh <jasonhsieh@Jasons-MacBook-Pro.local>
2026-04-27 17:37:13 +0800

    0427

--- a.v
+++ a.v
@@ -30,74 +30,7 @@ module LK (
     reg signed [8:0] It_now;
     wire [15:0] Ix_now2 = Ix_now * Ix_now;
     wire [15:0] Iy_now2 = Iy_now * Iy_now;
-    //================================沒用
-    wire signed [8:0] Ix_0 = Ix[0];
-    wire signed [8:0] Ix_1 = Ix[1];
-    wire signed [8:0] Ix_2 = Ix[2];
-    wire signed [8:0] Ix_3 = Ix[3];
-    wire signed [8:0] Ix_4 = Ix[4];
-    wire signed [8:0] Ix_5 = Ix[5];
-    wire signed [8:0] Ix_6 = Ix[6];
-    wire signed [8:0] Ix_7 = Ix[7];
-    wire signed [8:0] Ix_8 = Ix[8]; 
-    wire signed [8:0] Ix_9 = Ix[9];
-    wire signed [8:0] Ix_10 = Ix[10];
-    wire signed [8:0] Ix_11 = Ix[11];
-    wire signed [8:0] Ix_12 = Ix[12];
-    wire signed [8:0] Ix_13 = Ix[13];
-    wire signed [8:0] Ix_14 = Ix[14];
-    wire [7:0] img1_0 = img1[0];
-    wire [7:0] img1_1 = img1[1];
-    wire [7:0] img1_2 = img1[2];
-    wire [7:0] img1_3 = img1[3];
-    wire [7:0] img1_4 = img1[4];
-    wire [7:0] img1_5 = img1[5];
-    wire [7:0] img1_6 = img1[6];
-    wire [7:0] img1_7 = img1[7];
-    wire [7:0] img1_8 = img1[8];
-    wire [7:0] img1_9 = img1[9];
-    wire [7:0] img1_10 = img1[10];
-    wire [7:0] img1_11 = img1[11];  
-    wire [7:0] img1_12 = img1[12];
-    wire [7:0] img1_13 = img1[13];
-    wire [7:0] img1_14 = img1[14];
-    wire [7:0] img1_15 = img1[15];
-    wire [7:0] img1_16 = img1[16];
-    wire [8:0] It_0 = It[0];
-    wire [8:0] It_1 = It[1];
-    wire [8:0] It_2 = It[2];
-    wire [8:0] It_3 = It[3];
-    wire [8:0] It_4 = It[4];
-    wire [8:0] It_5 = It[5];
-    wire [8:0] It_6 = It[6];
-    wire [8:0] It_7 = It[7];
-    wire [8:0] It_8 = It[8];
-    wire [8:0] It_9 = It[9];
-    wire [8:0] It_10 = It[10];
-    wire [8:0] It_11 = It[11];  
-    wire [8:0] It_12 = It[12];
-    wire [8:0] It_13 = It[13];
-    wire [8:0] It_14 = It[14];
-    wire [8:0] It_15 = It[15];
-    wire [8:0] It_16 = It[16];
-    wire [8:0] Iy_0 = Iy[0];
-    wire [8:0] Iy_1 = Iy[1];
-    wire [8:0] Iy_2 = Iy[2];
-    wire [8:0] Iy_3 = Iy[3];
-    wire [8:0] Iy_4 = Iy[4];
-    wire [8:0] Iy_5 = Iy[5];
-    wire [8:0] Iy_6 = Iy[6];
-    wire [8:0] Iy_7 = Iy[7];
-    wire [8:0] Iy_8 = Iy[8];
-    wire [8:0] Iy_9 = Iy[9];
-    wire [8:0] Iy_10 = Iy[10];
-    wire [8:0] Iy_11 = Iy[11];
-    wire [8:0] Iy_12 = Iy[12];
-    wire [8:0] Iy_13 = Iy[13];
-    wire [8:0] Iy_14 = Iy[14];
-    wire [8:0] Iy_15 = Iy[15];
-    wire [8:0] Iy_16 = Iy[16];
-//================================
+
 
 // wire Ix_en = (col_reg !=1) && (col_reg !=0) && (row_reg !=0 && row_reg !=6); //什麼時候要計算 Ix
 // wire Iy_en = (col_reg !=6) && (col_reg !=0) && (row_reg !=0 && row_reg !=1); //什麼時候要計算 Iy
@@ -233,7 +166,7 @@ module tb;
     always #`HCYCLE clk = ~clk; // Clock generation
 integer i;
     initial begin
-        $readmemh("input.txt", data_mem);
+        $readmemh("input_combined.txt", data_mem);
         rst_n = 0; // Reset active
 
         #`CYCLE; // Wait for reset to be released
