b1161a96bc5bcb312838a58d99cbf3e21f18403b
Jason Hsieh <jasonhsieh@Jasons-MacBook-Pro.local>
2026-05-13 01:11:24 +0800

    0512

--- 01_RTL/a.v
+++ 01_RTL/a.v
@@ -10,8 +10,8 @@ module LK #(parameter width = 8)(
     input [width-1:0] a,
     input [width-1:0] b,
     output valid,
-    output reg [7:0] Vx,
-    output reg [7:0] Vy
+    output reg [11:0] Vx,
+    output reg [11:0] Vy
 );
     // reg [7:0] img1[0:48] ;
     reg [width-1:0] img1[0:13] ;
@@ -34,9 +34,6 @@ module LK #(parameter width = 8)(
     wire [width*2+1:0] Ix_now2 = Ix_now * Ix_now;//18 = 9+9
     wire [width*2+1:0] Iy_now2 = Iy_now * Iy_now;
 
-// wire Ix_en = (col_reg !=1) && (col_reg !=0) && (row_reg !=0 && row_reg !=6); //什麼時候要計算 Ix
-// wire Iy_en = (col_reg !=6) && (col_reg !=0) && (row_reg !=0 && row_reg !=1); //什麼時候要計算 Iy
-// wire It_en = (col_reg !=6) && (col_reg !=0) && (row_reg !=0 && row_reg !=6); //什麼時候要計算 It
 wire Ix_shift = (col_reg !=0) && (row_reg !=0); //什麼時候要 shift Ix
 wire Ix_en = Ix_shift && (col_reg !=1) && (row_reg !=6); //什麼時候要計算 Ix^2 IxIt
 wire Iy_en = (col_reg !=6) && (col_reg !=0) && (row_reg !=0 && row_reg !=1); //什麼時候要計算 Iy^2 IxIy IyIt 
@@ -68,33 +65,33 @@ always @(posedge clk or negedge rst_n) begin
         mul_src <= 0;
     end else begin
         mul_counter <= mul_counter + 1;
-        if(col_reg == 6 && row_reg == 6) begin 
-            case(mul_counter) 
-                3'd0: mul_src <= Ix2;
-                3'd1: mul_src <= Iy2;
-                3'd2: mul_src <= IxIy;
-                3'd3: mul_src <= IxIt;
-                3'd4: mul_src <= IyIt;
+            case(col_reg) 
+                3'd5: mul_src <= Ix2;
+                3'd6: mul_src <= Iy2;
+                3'd0: mul_src <= IxIy;
+                3'd1: mul_src <= IxIt;
+                3'd2: mul_src <= IyIt;
             endcase
-        end
+        
     end    
 end
 always @(posedge clk or negedge rst_n) begin 
-    if(~rst_n) begin
+    if(~rst_n || (col_reg == 5 && row_reg == 6)) begin
         mul_pos_buffer <= 0;
     end
     else begin 
-        if(~in_en) begin 
+
             mul_pos_buffer <= mul_pos_new;
-        end
+
     end
 end
 wire sum_shift = (mul_pos_buffer > 14);
-wire signed[2*width-1:0] Ix2_shift = (sum_shift)? (Ix2 >>> (mul_pos_buffer - 14)) : Ix2;
-wire signed[2*width-1:0] Iy2_shift = (sum_shift)? (Iy2 >>> (mul_pos_buffer - 14)) : Iy2;
-wire signed[2*width-1:0] IxIy_shift = (sum_shift)? (IxIy >>> (mul_pos_buffer - 14)) : IxIy;
-wire signed[2*width-1:0] IxIt_shift = (sum_shift)? (IxIt >>> (mul_pos_buffer - 14)) : IxIt;
-wire signed[2*width-1:0] IyIt_shift = (sum_shift)? (IyIt >>> (mul_pos_buffer - 14)) : IyIt;
+wire [3:0] shift_amount = (sum_shift)? (mul_pos_buffer - 14) : 0;
+wire signed[2*width-1:0] Ix2_shift = (Ix2 >>> shift_amount);
+wire signed[2*width-1:0] Iy2_shift =  (Iy2 >>> shift_amount) ;
+wire signed[2*width-1:0] IxIy_shift = (IxIy >>> shift_amount) ;
+wire signed[2*width-1:0] IxIt_shift = (IxIt >>> shift_amount) ;
+wire signed[2*width-1:0] IyIt_shift = (IyIt >>> shift_amount) ;
 wire signed [4*width:0] Ux = -(Iy2_shift * IxIt_shift) + (IxIy_shift * IyIt_shift); //-(197316*36516)+(-156086*-15534) =-4780551168
 wire signed [4*width:0] Uy = -(Ix2_shift * IyIt_shift)+ (IxIy_shift * IxIt_shift);//-(341126*-15534) + (-156086*36516)
 wire signed [4*width:0] det = (Ix2_shift * Iy2_shift) - (IxIy_shift * IxIy_shift);
@@ -105,12 +102,12 @@ wire [4*width:0] det_abs = det[4*width]? (-det) : det;
 wire [$clog2(4*width+1)-1 : 0] div_pos;
 wire div_valid;
 LOD #(.W(4*width+1)) L1(.in(det_abs), .pos(div_pos), .valid(div_valid));
-wire signed [4*width+20:0] Ux_pad = {{3{Ux[4*width+13]}}, Ux, 4'b0};
-wire signed [4*width+20:0] Uy_pad = {{3{Uy[4*width+13]}}, Uy, 4'b0};
-wire signed [4*width+20:0] shifted_x = Ux_pad >>> div_pos;
-wire signed [4*width+20:0] shifted_y = Uy_pad >>> div_pos;
-wire signed [7 : 0] result_x = (det[4*width+13])? -$signed(shifted_x[7 : 0]) : $signed(shifted_x[7 : 0]);
-wire signed [7 : 0] result_y = (det[4*width+13])? -$signed(shifted_y[7 : 0]) : $signed(shifted_y[7 : 0]);
+wire signed [4*width+11:0] Ux_pad = {{3{Ux[4*width]}}, Ux, 8'b0};
+wire signed [4*width+11:0] Uy_pad = {{3{Uy[4*width]}}, Uy, 8'b0};
+wire signed [4*width+11:0] shifted_x = Ux_pad >>> div_pos;
+wire signed [4*width+11:0] shifted_y = Uy_pad >>> div_pos;
+wire signed [11 : 0] result_x = (det[4*width])? -$signed(shifted_x[11 : 0]) : $signed(shifted_x[11 : 0]);
+wire signed [11 : 0] result_y = (det[4*width])? -$signed(shifted_y[11 : 0]) : $signed(shifted_y[11 : 0]);
 wire corner;
 Harris #(.width(width)) H1(.Ix2(Ix2_shift),.Iy2(Iy2_shift),.det(det),.corner(corner));
 always @(*) begin 
@@ -163,7 +160,7 @@ end
 
 // summaiton
 always @(posedge clk or negedge rst_n) begin
-    if(~rst_n) begin
+    if(~rst_n || (col_reg == 6 && row_reg == 0)) begin
         Iy2 <= 0;
         Ix2 <= 0;
         IxIt <= 0;
@@ -191,8 +188,8 @@ always @(posedge clk or negedge rst_n) begin
 
         if (col_reg == 6) begin // 0 到 6 代表 7 個數
             if(row_reg == 6) begin
-                col_reg <= 6;
-                row_reg <= 6;
+                col_reg <= 0;
+                row_reg <= 0;
             end
             else begin
                 col_reg <= 0;
@@ -259,7 +256,7 @@ module Harris#(parameter  width = 8)(
     wire signed [4*width+1:0] R;
     assign R = det - (trace_sq >>> 4);
 
-    parameter signed THRESHOLD = 32'd1000000000;
+    parameter signed THRESHOLD = 32'd10000;
 
     assign corner = (R > THRESHOLD);
 
@@ -272,7 +269,7 @@ module tb;
     reg [7:0] a;
     reg [7:0] b;
     wire [15:0] c;
-    reg [15:0] data_mem [0:48];
+    reg [15:0] data_mem [0:97];
     reg in_en;
     LK uut (
         .clk(clk),
@@ -302,11 +299,11 @@ integer i;
         #`CYCLE; // Wait for reset to be released
         in_en = 1;
         rst_n = 1; // Release reset
-        for (i = 0; i < 49; i = i + 1) begin
+        for (i = 0; i < 98; i = i + 1) begin
                 {a, b} = data_mem[i];
                 #`CYCLE;   // 一個 cycle
             end
-        
+
 
 
 
